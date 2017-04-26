f301b2d7330048fd1e940c4da5c048231d498bf4
Naren Dasan <naren@narendasan.com>
2017-04-26 11:40:21 -0700

    Create go-get-package-and-dependencies.go

---
+++ _til/go/go-get-package-and-dependencies.go
@@ -0,0 +1,13 @@
+--- 
+title: ```go get/install``` a Package and it's Dependencies at the same time 
+category: go
+---
+
+Packages may have a large amount of dependencies and so instead of forcing users to install them one by one 
+go's get tool provides a syntax to get and install a package and its dependencies in a single command
+
+```sh
+go get -u github.com/acm-uiuc/arbor/...
+```
+
+This command should get and install ```arbor``` and its dependencies
